4fe876f484ffc092f311af4eb8817f82590d15c8
shuaiwang01 <shuaiwang01@clubfactory.com>
2021-07-21 17:39:50 +0800

    fix 升级依赖版本导致报错

--- src/main/java/com/wangshuai/crawler/common/config/RestTemplateConfig.java
+++ src/main/java/com/wangshuai/crawler/common/config/RestTemplateConfig.java
@@ -12,12 +12,12 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.*;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
@@ -75,10 +75,12 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory clientHttpRequestFactory) {
         // 解决请求乱码
-        StringHttpMessageConverter m = new StringHttpMessageConverter(StandardCharsets.UTF_8);
-        RestTemplate restTemplate = new RestTemplateBuilder().additionalMessageConverters(m).build();
+        HttpMessageConverter m = new StringHttpMessageConverter(StandardCharsets.UTF_8);
+        RestTemplate restTemplate = new RestTemplate();
+        List<HttpMessageConverter<?>> httpMessageConverters = new ArrayList<>(restTemplate.getMessageConverters());
+        httpMessageConverters.add(m);
+        restTemplate.setMessageConverters(httpMessageConverters);
         restTemplate.setRequestFactory(clientHttpRequestFactory);
-        // restTemplate.setErrorHandler(new CustomResponseErrorHandler());
         List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
         interceptors.add(new CustomRequestInterceptor());
         restTemplate.setInterceptors(interceptors);
@@ -139,41 +141,5 @@ public class RestTemplateConfig {
             }
         }
     }
-
-    // private class CustomResponseErrorHandler extends DefaultResponseErrorHandler {
-    // @Override
-    // public void handleError(ClientHttpResponse response) throws IOException {
-    // HttpStatus statusCode = HttpStatus.resolve(response.getRawStatusCode());
-    // if (statusCode == null) {
-    // Charset charset = getCharset(response) == null ? StandardCharsets.UTF_8 : getCharset(response);
-    // String respBody = new String(getResponseBody(response), charset);
-    // this.logError(response.getRawStatusCode(), respBody);
-    // throw new UnknownHttpStatusCodeException(response.getRawStatusCode(), response.getStatusText(),
-    // response.getHeaders(), respBody.getBytes(charset), charset);
-    // }
-    // handleError(response, statusCode);
-    // }
-    //
-    // protected void handleError(ClientHttpResponse response, HttpStatus statusCode) throws IOException {
-    // Charset charset = getCharset(response) == null ? StandardCharsets.UTF_8 : getCharset(response);
-    // String respBody = new String(getResponseBody(response), charset);
-    // this.logError(statusCode.value(), respBody);
-    // switch (statusCode.series()) {
-    // case CLIENT_ERROR:
-    // throw new HttpClientErrorException(statusCode, response.getStatusText(),
-    // response.getHeaders(), respBody.getBytes(charset), charset);
-    // case SERVER_ERROR:
-    // throw new HttpServerErrorException(statusCode, response.getStatusText(),
-    // response.getHeaders(), respBody.getBytes(charset), charset);
-    // default:
-    // throw new UnknownHttpStatusCodeException(statusCode.value(), response.getStatusText(),
-    // response.getHeaders(), respBody.getBytes(charset), charset);
-    // }
-    // }
-    //
-    // private void logError(int statusCode, String respBody) {
-    // log.error("http 请求出错, http code : {}, response body : {}", statusCode, respBody);
-    // }
-    // }
 
 }
